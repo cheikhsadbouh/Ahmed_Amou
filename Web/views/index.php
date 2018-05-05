1bb90da56aadea3481c1a02c8b155393f1f5c930
cheikh <cheikhsaadbuh24@gmail.com>
2018-05-05 18:07:53 +0200

    design homepage 100 %  done :)

--- Web/views/index.php
+++ Web/views/index.php
@@ -204,7 +204,92 @@ ini_set('display_errors', 1);
 
     <!-- Page Content -->
     <div class="container">
+        <div class="gtco-section border-bottom">
+            <div class="gtco-container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+                        <h2>Beautiful Images</h2>
+                        <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/14THVISA2.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_2.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/img_3.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_3.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/img_4.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_4.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
 
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/img_1.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_1.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/img_5.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_5.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <a href="images/img_6.jpg" class="fh5co-project-item image-popup">
+                            <figure>
+                                <div class="overlay"><i class="ti-plus"></i></div>
+                                <img src="images/img_6.jpg" alt="Image" class="img-responsive">
+                            </figure>
+                            <div class="fh5co-text">
+                                <h2>Constructive heading</h2>
+                                <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
         <!-- Marketing Icons Section -->
         <div class="row">
             <div class="col-lg-12">
@@ -393,6 +478,8 @@ ini_set('display_errors', 1);
 
         </div>
     </div><!--end model-->
+
+
 
 </body>
 
